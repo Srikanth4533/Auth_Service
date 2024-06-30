3911c53e06803525d58be2b4dfb5a41b8a8a78d2
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-06-30 14:19:38 +0530

    Signup logic with password encryption is done

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,29 @@
+const UserService = require("../services/user-service")
+
+const userService = new UserService()
+
+const create = async (req, res) => {
+    try {
+        const user = await userService.create({
+            email: req.body.email,
+            password: req.body.password
+        })
+        return res.status(201).json({
+            data: user,
+            success: true,
+            message: "Successfully created a new user",
+            err: {}
+        })
+    } catch (error) {
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Something went wrong",
+            err: error
+        })
+    }
+}
+
+module.exports = {
+    create
+}
